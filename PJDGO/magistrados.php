63bdb9dd47ec19b4c949b61e8dc85fb2b559c288
Luis Luna <luishernandes2.jlh@gmail.com>
2023-06-01 11:15:11 -0600

    v7 header pjdgo

--- PJDGO/magistrados.php
+++ PJDGO/magistrados.php
@@ -1,10 +1,24 @@
+<?php
+include_once "../header.php";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/magistrados.css">
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&family=Roboto:wght@300;500;700&display=swap"
+        rel="stylesheet">
+        <link rel="shortcut icon" href="../img/logo1.jpg">
+        <link href="../css/style.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
     <title>Magistrados</title>
 </head>
 
@@ -14,7 +28,7 @@
 
 <center>
 <div class="image-container">
-  <img src="img/YOLANDA-DE-LA-TORRE-VALDEZ.jpg" alt="Magistrada"> 
+  <img src="../img/YOLANDA-DE-LA-TORRE-VALDEZ.jpg" alt="Magistrada"> 
     <div class="image-description">
     M.D YOLANDA DE LA TORRE VALDEZ
     <P>MAGISTRADA PRESIDENTA</P>
