1bff721bf246c034a05ca8758b9170d07a80fbcc
Marlenne <Marlenneibarr@gmail.com>
2023-06-01 11:13:12 -0600

    v7 magistrados

--- PJDGO/magistrados.php
+++ PJDGO/magistrados.php
@@ -1,26 +1,50 @@
+<?php
+ include_once "../header.php"
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/magistrados.css">
+
+    <!-- CONTENIDO DEL ENCABEZADO DE PAGINA O MENU DE NAVEGACION -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    <link rel="shortcut icon" href="../img/logo1.jpg">
+    <link href="../css/style.css" rel="stylesheet">
+
+    <link rel="stylesheet" href="../css/PJDGO.css">
+
+
     <title>Magistrados</title>
 </head>
 
 <body>
-<center><h1>Honorable Pleno del Tribunal Superior de Justicia</h1></center>
+    <br>
+    <br>
+    <center><h1>Honorable Pleno del Tribunal Superior de Justicia</h1></center>
 
+    <br>
+    <center>
+    <div class="image-container">
+      <img src="../img/YOLANDA-DE-LA-TORRE-VALDEZ.jpg">
+    </div>
+    </center>
 
-<center>
-<div class="image-container">
-  <img src="img/YOLANDA-DE-LA-TORRE-VALDEZ.jpg" alt="Magistrada"> 
-    <div class="image-description">
-    M.D YOLANDA DE LA TORRE VALDEZ
-    <P>MAGISTRADA PRESIDENTA</P>
-  </div>
-</div>
-</center>
+    <div class= "row" style="justify-content: center;" >
+    <div class="image-container col-lg-4">
+      <img src="ruta_de_la_imagen2.jpg" >
+    </div>
+      
+      <div class="image-container col-lg-4" >
+        <img src="ruta_de_la_imagen3.jpg">
+    </div>
+
+    <div class="image-container col-lg-4">
+        <img src="ruta_de_la_imagen3.jpg">
+    </div>
+    </div>
 
 </body>
 </html>
