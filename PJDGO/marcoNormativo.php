033ae9dd9112ceea4e58dadbf793a88341319dd9
Luis Luna <luishernandes2.jlh@gmail.com>
2023-06-15 14:13:55 -0600

    marco corregido

--- PJDGO/marcoNormativo.php
+++ PJDGO/marcoNormativo.php
@@ -1,5 +1,7 @@
 <?php
 include_once "pjdgo_header.php";
+include_once "librerias.php";
+
 ?>
 
 <!DOCTYPE html>
@@ -8,7 +10,6 @@ include_once "pjdgo_header.php";
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>MARCO NORMATIVO</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/MarcoNormativo.css">
 </head>
 
